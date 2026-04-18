8a5bb89afa55b787504e852b1edf32696c95c8f8
OfficerMello <quamidris@gmail.com>
2026-04-18 16:11:58 -0400

    q4.js

---
+++ q4.js
@@ -0,0 +1,13 @@
+// Question 4: Array operations - push and shift
+
+const fruits = ["mango", "orange", "banana"];
+
+// (i) Add 'pineapple' to the end
+fruits.push("pineapple");
+
+// (ii) Remove the first item
+fruits.shift();
+
+// (iii) Log the updated array and its length
+console.log(fruits);
+console.log(fruits.length);
